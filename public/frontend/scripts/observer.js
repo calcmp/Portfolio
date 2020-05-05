ffe89dced79ca9c7b49c68def0e4956d1459205c
calcmp <calcmp@gmail.com>
2020-05-05 15:10:17 +0100

    Changed threshold of fading and sliding

--- public/frontend/scripts/observer.js
+++ public/frontend/scripts/observer.js
@@ -1,26 +1,42 @@
 const faders = document.querySelectorAll(".fade-in");
 const sliders = document.querySelectorAll(".slide-in");
 
-const appearOptions = {
-  threshold: 0,
+const faderOptions = {
+  threshold: 0.5,
   rootMargin: "0px 0px -20px 0px",
 };
 
-const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+const sliderOptions = {
+  threshold: 0.2,
+  rootMargin: "0px 0px -20px 0px",
+};
+
+const fadeOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       return;
     } else {
       entry.target.classList.add("appear");
-      appearOnScroll.unobserve(entry.target);
+      fadeOnScroll.unobserve(entry.target);
     }
   });
-}, appearOptions);
+}, faderOptions);
+
+const slideOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      slideOnScroll.unobserve(entry.target);
+    }
+  });
+}, sliderOptions);
 
 faders.forEach((fader) => {
-  appearOnScroll.observe(fader);
+  fadeOnScroll.observe(fader);
 });
 
 sliders.forEach((slider) => {
-  appearOnScroll.observe(slider);
+  slideOnScroll.observe(slider);
 });
